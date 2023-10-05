f619849d2e72b34b9a2b069f5490203e279a4ddd
ssb000ss <ssb000ss@gmail.com>
2023-10-05 10:56:10 +0600

    Add example of using the engines core

--- example.py
+++ example.py
@@ -1,13 +1,13 @@
 def netlas_test(engines):
     engine_arg = '-n'
-    query = 'http.favicon.hash_sha256:50836e4dd679302c8477bb1cc3667a605f16d998709ef2f68ed1fb9d2be8ea7d AND geo.country:US'
+    query = 'http.favicon.hash_sha256:50836e4dd679302c8477bb1cc3667a605f16d998709ef2f68ed1fb9d2be8ea7d AND geo.country:SN'
     engine = engines.get(engine_arg)
     engine.search_and_save_ip_list(query)
 
 
 def zoomeye_test(engines):
     engine_arg = '-z'
-    query = 'app:"Microsoft Exchange 2010 smtpd" +after:"2022-10-03" +country:"UA"'
+    query = 'app:"Microsoft Exchange 2010 smtpd" +after:"2022-10-03" +country:"SN"'
     engine = engines.get(engine_arg)
     engine.count(query)
     engine.search_and_save_ip_list(query)
@@ -15,6 +15,6 @@ def zoomeye_test(engines):
 
 def fofa_test(engines):
     engine_arg = '-f'
-    query = 'icon_hash="2141724739" && country="KG"'
+    query = 'icon_hash="2141724739" && country="SN"'
     engine = engines.get(engine_arg)
     engine.search_and_save_ip_list(query)
